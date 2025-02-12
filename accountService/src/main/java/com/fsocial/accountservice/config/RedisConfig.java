49d1a4bcf4825d99000e49fbd9b9f2d66d0afcc4
CangNgo <cangngoo123@gmail.com>
2025-02-12 19:07:30 +0700

    checkout

--- accountService/src/main/java/com/fsocial/accountservice/config/RedisConfig.java
+++ accountService/src/main/java/com/fsocial/accountservice/config/RedisConfig.java
@@ -1,10 +1,7 @@
 package com.fsocial.accountservice.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;

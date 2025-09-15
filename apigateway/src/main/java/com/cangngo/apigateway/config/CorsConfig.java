582ee5ff93f62558e39b57821326c3e574b6a0df
CangNgo <cangngoo123@gmail.com>
2025-09-16 00:45:12 +0700

    fix:cors

--- apigateway/src/main/java/com/cangngo/apigateway/config/CorsConfig.java
+++ apigateway/src/main/java/com/cangngo/apigateway/config/CorsConfig.java
@@ -1,13 +1,10 @@
 package com.cangngo.apigateway.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
